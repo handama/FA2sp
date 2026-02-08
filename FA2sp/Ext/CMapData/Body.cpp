686f0448b3ff5882cb6c821f86b369ff94870144
handama <55939089+handama@users.noreply.github.com>
2026-02-08 14:41:58 +0800

    fix team validator

--- FA2sp/Ext/CMapData/Body.cpp
+++ FA2sp/Ext/CMapData/Body.cpp
@@ -3663,9 +3663,6 @@ void CMapDataExt::InitializeAllHdmEdition(bool updateMinimap, bool reloadCellDat
 	}
 
 	ppmfc::CString pInfoSection = TheaterInfo::GetInfoSection();
-	// Forward compatibility
-	if (CINI::FAData->SectionExists(pInfoSection + "2"))
-		pInfoSection += "2";
 	TheaterInfo::CurrentBigWaters.clear();
 	TheaterInfo::CurrentSmallWaters.clear();
 	if (CINI::FAData->KeyExists(pInfoSection, "BigWaterIndices"))

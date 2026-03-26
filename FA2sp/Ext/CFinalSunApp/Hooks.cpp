b7086ad64bb77ffa4b0168b3c73b49cf80786b66
handama <55939089+handama@users.noreply.github.com>
2026-03-26 22:48:33 +0800

    only response arrow key when focus is on isoview

--- FA2sp/Ext/CFinalSunApp/Hooks.cpp
+++ FA2sp/Ext/CFinalSunApp/Hooks.cpp
@@ -24,15 +24,20 @@ DEFINE_HOOK(41FAD0, CFinalSunApp_InitInstance, 8)
 DEFINE_HOOK(4229E0, CFinalSunApp_ProcessMessageFilter, 7)
 {
     if (!CMapData::Instance->MapWidthPlusHeight) return 0;
-
     REF_STACK(LPMSG, lpMsg, 0x8);
+    if (lpMsg->message != WM_KEYDOWN) return 0;
+
+    POINT pt;
+    GetCursorPos(&pt);
+    HWND hTopWnd = WindowFromPoint(pt);
+    if (hTopWnd != CIsoView::GetInstance()->GetSafeHwnd()) return 0;
 
     if (CViewObjectsExt::CurrentConnectedTileType < 0 
         || CViewObjectsExt::CurrentConnectedTileType > CViewObjectsExt::ConnectedTileSets.size()
         || CIsoView::CurrentCommand->Command != 0x1E)
     {
         auto& gov = GridObjectViewer::Instance;
-        if (lpMsg->message == WM_KEYDOWN && gov.IsVisible())
+        if (gov.IsVisible())
         {
             bool changed = false;
             switch (lpMsg->wParam)
@@ -43,11 +48,9 @@ DEFINE_HOOK(4229E0, CFinalSunApp_ProcessMessageFilter, 7)
             case VK_RIGHT:
                 changed = gov.SelectRight();
                 break;
-
             case VK_UP:
                 changed = gov.SelectUp();
                 break;
-
             case VK_DOWN:
                 changed = gov.SelectDown();
                 break;
@@ -56,13 +59,13 @@ DEFINE_HOOK(4229E0, CFinalSunApp_ProcessMessageFilter, 7)
             {
                 InvalidateRect(gov.GetView(), NULL, TRUE);
                 gov.EnsureVisible(gov.GetSelectedIndex());
-                gov.OnSelChanged(gov.GetSelectedIndex()); 
+                gov.OnSelChanged(gov.GetSelectedIndex());
 
                 POINT pt;
                 GetCursorPos(&pt);
                 ScreenToClient(CIsoView::GetInstance()->GetSafeHwnd(), &pt);
                 CIsoView::GetInstance()->OnMouseMove(0, pt);
-            }
+            }       
         }
     }
     else
@@ -70,40 +73,36 @@ DEFINE_HOOK(4229E0, CFinalSunApp_ProcessMessageFilter, 7)
         int currentCTtype = CViewObjectsExt::ConnectedTileSets[CViewObjectsExt::CurrentConnectedTileType].Type;
         if (!CViewObjectsExt::IsInPlaceCliff_OnMouseMove)
         {
-            if (lpMsg->message == WM_KEYDOWN)
+            auto point = CIsoView::GetInstance()->GetCurrentMapCoord(CIsoView::GetInstance()->MouseCurrentPosition);
+            if (lpMsg->wParam == VK_UP && (currentCTtype == CViewObjectsExt::DirtRoad || currentCTtype == CViewObjectsExt::CityDirtRoad || currentCTtype == CViewObjectsExt::Highway))
             {
-                auto point = CIsoView::GetInstance()->GetCurrentMapCoord(CIsoView::GetInstance()->MouseCurrentPosition);
-                if (lpMsg->wParam == VK_UP && (currentCTtype == CViewObjectsExt::DirtRoad || currentCTtype == CViewObjectsExt::CityDirtRoad || currentCTtype == CViewObjectsExt::Highway))
+                if (CViewObjectsExt::CliffConnectionHeight < 14 && CViewObjectsExt::LastPlacedCT.GetNextHeightOffset() < 1 || CViewObjectsExt::NextCTHeightOffset < 0 || CViewObjectsExt::CliffConnectionHeight < 13)
                 {
-                    if (CViewObjectsExt::CliffConnectionHeight < 14 && CViewObjectsExt::LastPlacedCT.GetNextHeightOffset() < 1 || CViewObjectsExt::NextCTHeightOffset < 0 || CViewObjectsExt::CliffConnectionHeight < 13)
-                    {
-                        CViewObjectsExt::RaiseNextCT();
-                        CViewObjectsExt::PlaceConnectedTile_OnMouseMove(point.X, point.Y, false);
-                    }
+                    CViewObjectsExt::RaiseNextCT();
+                    CViewObjectsExt::PlaceConnectedTile_OnMouseMove(point.X, point.Y, false);
+                }
 
-                }
-                else if (lpMsg->wParam == VK_DOWN && (currentCTtype == CViewObjectsExt::DirtRoad || currentCTtype == CViewObjectsExt::CityDirtRoad || currentCTtype == CViewObjectsExt::Highway))
+            }
+            else if (lpMsg->wParam == VK_DOWN && (currentCTtype == CViewObjectsExt::DirtRoad || currentCTtype == CViewObjectsExt::CityDirtRoad || currentCTtype == CViewObjectsExt::Highway))
+            {
+                if (CViewObjectsExt::CliffConnectionHeight > 0 || CViewObjectsExt::NextCTHeightOffset > 0 || CViewObjectsExt::CliffConnectionHeight == 0 && CViewObjectsExt::LastPlacedCT.GetNextHeightOffset() > 0)
                 {
-                    if (CViewObjectsExt::CliffConnectionHeight > 0 || CViewObjectsExt::NextCTHeightOffset > 0 || CViewObjectsExt::CliffConnectionHeight == 0 && CViewObjectsExt::LastPlacedCT.GetNextHeightOffset() > 0)
-                    {
-                        CViewObjectsExt::LowerNextCT();
-                        CViewObjectsExt::PlaceConnectedTile_OnMouseMove(point.X, point.Y, false);
-                    }
+                    CViewObjectsExt::LowerNextCT();
+                    CViewObjectsExt::PlaceConnectedTile_OnMouseMove(point.X, point.Y, false);
                 }
-                else if (lpMsg->wParam == VK_PRIOR)
+            }
+            else if (lpMsg->wParam == VK_PRIOR)
+            {
+                if (CViewObjectsExt::CliffConnectionHeight < 14)
                 {
-                    if (CViewObjectsExt::CliffConnectionHeight < 14)
-                    {
-                        CViewObjectsExt::CliffConnectionHeight++;
-                    }
-
+                    CViewObjectsExt::CliffConnectionHeight++;
                 }
-                else if (lpMsg->wParam == VK_NEXT)
+            }
+            else if (lpMsg->wParam == VK_NEXT)
+            {
+                if (CViewObjectsExt::CliffConnectionHeight > 0)
                 {
-                    if (CViewObjectsExt::CliffConnectionHeight > 0)
-                    {
-                        CViewObjectsExt::CliffConnectionHeight--;
-                    }
+                    CViewObjectsExt::CliffConnectionHeight--;
                 }
             }
         }

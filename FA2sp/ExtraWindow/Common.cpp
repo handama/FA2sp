96b04d7895e3c10bf599d05274f6c252e419a7d6
handama <55939089+handama@users.noreply.github.com>
2026-08-23 03:32:58 +0800

    fix float param bug in trigger editor

--- FA2sp/ExtraWindow/Common.cpp
+++ FA2sp/ExtraWindow/Common.cpp
@@ -326,8 +326,7 @@ void ExtraWindow::LoadParams(VirtualComboBoxEx& vcb, FString idx, CNewTrigger* i
         LoadParam_Tags(vcb);
         break;
     case 12: // float
-        if (instance == &CNewTrigger::Instance[0]) CNewTrigger::Instance[0].ActionParamUsesFloat = true;
-        else if (instance == &CNewTrigger::Instance[1]) CNewTrigger::Instance[1].ActionParamUsesFloat = true;
+        instance->ActionParamUsesFloat = true;
         break;
     case 13:
         LoadParam_CountryList(vcb);
